0a2124fa8d65192a78b2fa8f079602b7c26728c1
Kyler Berry <kyler.berry@gmail.com>
2015-03-15 14:17:54 -0400

    create Ball class with expander properties

--- src/ball.js
+++ src/ball.js
@@ -1,36 +1,17 @@
-function Expander() {
-	var size = view.size;
-/*
-	this.radius = 4;
-	this.point = Point.random();
-	this.point.x = this.point.x * size.width;
-	this.point.y = this.point.y * size.height;
-
-	this.vector = new Point({
-		angle: 360 * Math.random(),
-		length: speed
-	});
-
-
-	this.circle = new Path.Circle({
-		x: this.point.x,
-		y: this.point.y
-	}, this.radius);
-	this.circle.fillColor = 'red';*/
-
-	this.expander = new Path.Circle({
-					x: event.point.x,
-					y: event.point.y
-				}, 5);
-	expander.fillColor = '#7fddc0';
+function Ball(expander) {
+  this.point = expander.point;
+	this.ball = new Path.Circle({
+					x: this.point.x,
+					y: this.point.y
+				}, expander.expander.bounds.width / 2);
+	this.ball.strokeColor = '#eee';
+	this.ball.strokeWidth = 1;
 }
 
-Expander.prototype = {
-/*	iterate: function() {
-		this.checkBorders();
-		this.move();
+Ball.prototype = {
+	iterate: function() {
 	},
-	checkBorders: function() {
+	/*checkBorders: function() {
 		size = view.size;
 
 		if(this.point.x < this.radius) {
